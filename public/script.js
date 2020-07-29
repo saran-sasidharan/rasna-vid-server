aa2f03642da1b33f1c5b1e25337c9bd7b19ba929
Saran Sasidharan <saran.s.dhar@gmail.com>
2020-07-30 03:18:01 +0530

    Updated deployed peer server details to the app

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
+  host: 'rasna-vid-peer.herokuapp.com',
+  port: '80'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
